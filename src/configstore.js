c4db349d657d7f69bbb7d0f2c27e19f3a474622e
sashank12-hub <58355991+sashank12-hub@users.noreply.github.com>
2021-01-08 21:21:46 +0530

    Updates

--- src/configstore.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import {productreducer} from './reducer/productreducer';
-
-export const configstore=()=>{
-    const store=createStore(productreducer);
-    return store;
-}
